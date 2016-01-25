bb6c83508b3076130f31bd85f478e0b64ea32ef7
sam3killa <samuels.shih@gmail.com>
2016-01-25 13:19:39 -0800

    Initial Commit

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Samuel Shih on 1/25/16.
+//  Copyright © 2016 Samuel Shih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
